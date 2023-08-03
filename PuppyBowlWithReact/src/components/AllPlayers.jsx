1ed6f64c6610e8ebf1dd6a6019103034daa2d809
AmairT <amair0829@gmail.com>
2023-08-02 21:09:07 -0700

    added all players to website

--- PuppyBowlWithReact/src/components/AllPlayers.jsx
+++ PuppyBowlWithReact/src/components/AllPlayers.jsx
@@ -1,7 +1,38 @@
+import ajaxHelpers from '../API/ajaxHelpers'
+import { useNavigate } from 'react-router-dom'
+import SinglePlayer from './SinglePlayer'
+import NewPlayerForm from './NewPlayerForm'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+
 export default function AllPlayers() {
+    const [players, setPlayers] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const testPlayers = async() => {
+            const playerArray = await ajaxHelpers()
+            setPlayers(playerArray.data.players);
+        };
+        testPlayers();
+    },[players])
+
     return (
-        <>
-            <h2>The All Players Component</h2>
-        </>
-    )    
+    <>
+        <div className='container'>
+            {players.map((player) => {
+                return (
+                    <div className='allPlayers' key={player.id}>
+                        <h4>Hi, my name is {player.name}!</h4>
+                        <img src='{player.imageUrl}' alt='' />
+                        <button onClick={() => navigate(`/players/${player.id}`)}>See Details</button>
+                    </div>
+                )
+            })
+            }
+            <NewPlayerForm />
+        </div>
+    </>
+    )
 }
